f5aa9c6977fecd16c28231cb933e8eba20e394b0
Wilhelm Alan <alan.wilhelm7@etu.univ-lorraine.fr>
2023-12-19 15:12:41 +0100

    failing test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -3,6 +3,5 @@ const Tom = require("../main");
 describe("Checking if the user is created correctly", function () {
  it("should create the user with the correct name", function () {
  const tom = new Tom();
- tom.name.should.be.equal("Tom");
- });
+ tom.name.should.be.equal("tommy"); });
 });
