ce5c8cafa75c41707d5670a87d0a9df3df510b01
Wilhelm Alan <alan.wilhelm7@etu.univ-lorraine.fr>
2023-12-19 15:01:35 +0100

    skeleton + tests

---
+++ main.js
@@ -0,0 +1,7 @@
+class Tom {
+    constructor() {
+    this.name = "Tom"; // Tom's name is Tom
+    this.money = 0.0; // Tom has no money
+    }
+   }
+   module.exports = Tom;
---
+++ tests/demo.test.js
@@ -0,0 +1,8 @@
+const should = require("should");
+const Tom = require("../main");
+describe("Checking if the user is created correctly", function () {
+ it("should create the user with the correct name", function () {
+ const tom = new Tom();
+ tom.name.should.be.equal("Tom");
+ });
+});
